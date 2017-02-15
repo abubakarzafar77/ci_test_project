b8918ef1a05ef2e31b6c11f4a98bd956fb8f14d5
abubakarzafar77 <abubakar.zafar@purelogics.net>
2017-02-16 01:28:43 +0500

    base contoller and admin and user

---
+++ application/controllers/admin.php
@@ -0,0 +1,10 @@
+<?php 
+
+
+class Admin extends MY_Controller
+{
+	public function index()
+	{
+		echo "this index function of admin controller";
+	}
+}
---
+++ application/controllers/user.php
@@ -0,0 +1,9 @@
+<?php 
+
+class User extends MY_Controller
+{
+	public function index()
+	{
+		echo "this is index function of User Controller ";
+	}
+}
